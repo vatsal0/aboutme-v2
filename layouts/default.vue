a568ecdbd00c1ada256d5dd584016418bb726042
Vatsal Baherwani <34689545+vatsal0@users.noreply.github.com>
2021-12-19 14:28:06 -0500

    General info, contact, sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      :mini-variant="miniVariant"
+      clipped
       fixed
       app
+      expand-on-hover
+      mobile-breakpoint="sm"
     >
-      
       <v-list>
         <v-list-item
           to="/"
@@ -13,18 +14,16 @@
           exact
         >
           <v-list-item-action>
-            <v-avatar
-              size="24px"
-            >
-              <img
-                src="headshot.jpeg"
-              >
+            <v-avatar size="24px">
+              <img src="headshot.jpeg">
             </v-avatar>
           </v-list-item-action>
+
           <v-list-item-content>
             <v-list-item-title v-text="'About Me'" />
           </v-list-item-content>
         </v-list-item>
+
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -35,27 +34,30 @@
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
+
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      fixed
+
+    <!-- <v-app-bar
+      clipped-left
       app
     >
       <v-btn
         icon
-        @click.stop="miniVariant = !miniVariant"
+        @click.self.stop="miniVariant = !miniVariant"
       >
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
-    </v-app-bar>
+    </v-app-bar> -->
+
     <v-main>
       <v-container>
-        <Nuxt />
+        <Nuxt class="d-flex flex-column fill-height"/>
       </v-container>
     </v-main>
   </v-app>
@@ -83,9 +85,14 @@ export default {
           to: '/#projects'
         }
       ],
-      miniVariant: false,
       title: 'Vatsal Baherwani'
     }
   }
 }
 </script>
+
+<style>
+html {
+  scroll-behavior:smooth;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,89 @@
 <template>
-	<div>
-		Placeholder text
-	</div>
+<div>
+	<v-container fluid style="min-height:100vh">
+		<v-row justify="center" width="100%">
+			<v-col cols="12" sm="8">
+				<v-card>
+					<h1 class="py-4 text-wrap text-center">{{title}}</h1>
+					<v-card-title class="headline d-flex flex-column justify-center">
+						<v-avatar size="16rem" class="mx-auto mb-4">
+							<img src="headshot.jpeg">
+						</v-avatar>
+						Welcome to my page!
+					</v-card-title>
+
+					<v-card-text class="text-center">
+						<p>I'm an undergraduate computer science student interested in machine learning and finance. Currently, I am a freshman at the University of Maryland. You can view my resume <a
+								href="resume.pdf"
+								target="_blank"
+							>here</a>.</p>
+						
+						<p>
+							Click on the links on the sidebar to learn more about me!
+						</p>
+
+						<hr class="my-3">
+					</v-card-text>
+					<v-card-actions>
+						<div class="d-flex flex-column flex-lg-row justify-space-around align-center flex-grow-1">
+							<v-btn 
+								:href="link.to"
+								target="_blank"
+								v-for="(link, i) in links"
+								:key="i"
+								class="my-2 my-lg-auto d-flex"
+							>
+								<v-icon>{{link.icon}}</v-icon>
+								<p
+									class="ml-2 my-auto text-caption"
+								>{{link.title}}</p>
+							</v-btn>
+						</div>
+							
+					</v-card-actions>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
+
+	<v-container fluid style="min-height:100vh" id="experience">
+		<h1 class="text-center">Experience</h1>
+	</v-container>
+
+	<v-container fluid style="min-height:100vh" id="education">
+		<h1 class="text-center">Education</h1>
+	</v-container>
+
+	<v-container fluid style="min-height:100vh" id="projects">
+		<h1 class="text-center">Projects</h1>
+	</v-container>
+</div>
 </template>
+
+<script>
+export default {
+	data () {
+    return {
+      links: [
+        {
+          icon: 'mdi-email',
+          title: 'Send me an email',
+          to: 'mailto:vatsalbaherwani@gmail.com'
+        },
+        {
+          icon: 'mdi-linkedin',
+          title: 'Connect with me on linkedin',
+          to: 'https://www.linkedin.com/in/vatsal-baherwani'
+        },
+        {
+          icon: 'mdi-github',
+          title: 'View my projects on GitHub',
+          to: 'https://www.github.com/vatsal0'
+        }
+      ],
+      miniVariant: false,
+      title: 'Vatsal Baherwani'
+    }
+  }
+}
+</script>
