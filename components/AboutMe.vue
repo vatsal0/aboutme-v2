db832c2ad2b06b51689efd10a2d9e91141ec5f90
Vatsal Baherwani <34689545+vatsal0@users.noreply.github.com>
2021-12-19 17:12:42 -0500

    Experience section

---
+++ components/AboutMe.vue
@@ -0,0 +1,73 @@
+<template>
+	<v-container fluid style="min-height:100vh">
+		<v-row justify="center" width="100%" class="my-16">
+			<v-col cols="12" sm="8">
+				<v-card class="my-auto">
+					<h1 class="py-4 text-wrap text-center">{{title}}</h1>
+					<v-card-title class="headline d-flex flex-column justify-center">
+						<v-avatar size="16rem" class="mx-auto mb-4">
+							<img src="headshot.jpeg">
+						</v-avatar>
+						Welcome to my page!
+					</v-card-title>
+
+					<v-card-text class="text-center">
+						<p>I'm an undergraduate computer science student interested in machine learning and finance. Currently, I am a freshman at the University of Maryland. You can view my resume <a
+								href="resume.pdf"
+								target="_blank"
+							>here</a>.</p>
+						
+						<p>
+							Click on the links on the sidebar to learn more about me!
+						</p>
+
+						<hr class="my-3">
+					</v-card-text>
+					<v-card-actions>
+						<div class="d-flex flex-column flex-lg-row justify-space-around align-center flex-grow-1">
+							<v-btn 
+								:href="link.to"
+								target="_blank"
+								v-for="(link, i) in links"
+								:key="i"
+								class="my-2 my-lg-auto d-flex"
+							>
+								<v-icon>{{link.icon}}</v-icon>
+								<p
+									class="ml-2 my-auto text-caption"
+								>{{link.title}}</p>
+							</v-btn>
+						</div>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+export default {
+	data () {
+    return {
+      links: [
+        {
+          icon: 'mdi-email',
+          title: 'Send me an email',
+          to: 'mailto:vatsalbaherwani@gmail.com'
+        },
+        {
+          icon: 'mdi-linkedin',
+          title: 'Connect with me on linkedin',
+          to: 'https://www.linkedin.com/in/vatsal-baherwani'
+        },
+        {
+          icon: 'mdi-github',
+          title: 'View my projects on GitHub',
+          to: 'https://www.github.com/vatsal0'
+        }
+      ],
+      title: 'Vatsal Baherwani'
+    }
+  }
+}
+</script>
---
+++ components/Experience.vue
@@ -0,0 +1,78 @@
+<template>
+	<v-container fluid style="min-height:100vh; width: 100%" id="experience">
+		<h1 class="text-center">Experience</h1>
+
+		<div>
+			<template>
+				<v-timeline>
+					<v-timeline-item
+						v-for="(item, i) in items"
+						:key="i"
+						large
+					>
+						<template v-slot:icon>
+							<v-avatar>
+								<img :src="item.img">
+							</v-avatar>
+						</template>
+						
+						<v-card class="elevation-2">
+							<v-card-title class="text-h4 pt-2 pb-0">
+								{{item.title}}
+							</v-card-title>
+
+							<v-card-title class="overline py-0">
+								{{item.time}}
+							</v-card-title>
+
+							<v-card-text>
+								<v-list-item
+								v-for="(line, i) in item.desc"
+								:key="i">
+									<v-list-item-content>
+										<v-list-item-title class="text-wrap">{{line}}</v-list-item-title>
+									</v-list-item-content>
+								</v-list-item>
+							</v-card-text>
+						</v-card>
+					</v-timeline-item>
+				</v-timeline>
+			</template>
+		</div>
+	</v-container>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			items: [
+				{
+					time: "Nov 2021 - Present",
+					title: "Research Assistant",
+					place: "University of Maryland",
+					desc: ["Working under Dr. Lars Olson in the Department of Agriculture and Resource Economics.",
+						"Utilizing a web scraper to collect invasive species data from the CABI Invasive Species Compendium.",
+						"Developing a natural language processing model to extract insights related to the economic impact of\
+						invasive species and identify the most prominent economic threats."],
+					img: "https://osc.umd.edu/img/logos/28_informalseal.jpg"
+				},
+				{
+					time: "Sep 2020 - Aug 2021",
+					title: "Software Engineer Intern",
+					place: "Bloomberg LP",
+					desc: ["Developed a RESTful service using Java and Spring Framework to\
+					manage over 900 permissions and settings used by the Bloomberg Law web application.",
+					"Created a responsive and dynamic interface with Vue.js, GraphQL, and\
+					Ruby on Rails to communicate with the Java service via HTTP requests.",
+					"Provided analytics allowing managers to recognize and remove hundreds\
+					of unused permissions, reducing login time by over 200ms.",
+					"Presented interface in front of managers and senior engineers to receive\
+					feedback for implementing new features and improving user experience."],
+					img: "https://www.logolynx.com/images/logolynx/57/5749ace331ebd85068ef14fcdc115285.jpeg"
+				}
+			]
+		}
+	}
+}
+</script>
